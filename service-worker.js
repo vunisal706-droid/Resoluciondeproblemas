7b1800ee47fe73fd99c5cb04f5c95949afe1651f
vunisal706-droid <vunisal706@gmail.com>
2025-10-25 18:17:51 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,86 +1,25 @@
-const CACHE_NAME = 'matematicas-ceip-v1';
+const CACHE_NAME = 'problemas-matematicos-v2';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/apple-touch-icon.png',
-  'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
-  'https://unpkg.com/@babel/standalone/babel.min.js',
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/icons/apple-touch-icon.png',
   'https://cdn.tailwindcss.com',
-  'https://unpkg.com/lucide@latest'
+  'https://raw.githubusercontent.com/vunisal706-droid/Resoluciondeproblemas/main/LISTA-PROBLEMAS.txt'
 ];
 
-// Instalación del Service Worker
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Caché abierto');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-// Activación del Service Worker
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Eliminando caché antigua:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-// Interceptar peticiones
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        // Si está en caché, devuelve la respuesta
-        if (response) {
-          return response;
-        }
-
-        // Si no está en caché, hace la petición a la red
-        return fetch(event.request)
-          .then((response) => {
-            // Verifica que la respuesta es válida
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // Clona la respuesta
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          })
-          .catch(() => {
-            // Si falla la red, intenta devolver desde caché
-            return caches.match('./index.html');
-          });
-      })
+      .then(response => response || fetch(event.request))
   );
-});
-
-// Manejo de mensajes
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
 });
